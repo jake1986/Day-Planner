a408407a0e569b0755ea87ce6901a6cec16a7dd0
Jake Rhoads <jake.rhoads@gmail.com>
2020-03-10 13:04:55 -0400

    initial commit for Day Planner

---
+++ app.js
@@ -0,0 +1,13 @@
+// document.ready(function ()
+// {      
+//    var makeEvent = document.querySelector(".makeEvent");
+
+//    var commitBtn = document.querySelector("#commit");
+
+//    commitBtn.addEventListener("click", function() {
+//        console.log("I am being clicked");
+//    })
+
+// });
+
+
