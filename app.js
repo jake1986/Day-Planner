d439a32b028a6b04c0c9a6e9f1590f0fb58cb4c0
Jake Rhoads <jake.rhoads@gmail.com>
2020-03-11 18:10:25 -0400

    css contains added changes to col-2 with radius and purple

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-// document.ready(function ()
-// {      
-//    var makeEvent = document.querySelector(".makeEvent");
-
-//    var commitBtn = document.querySelector("#commit");
-
-//    commitBtn.addEventListener("click", function() {
-//        console.log("I am being clicked");
-//    })
-
-// });
-
 
